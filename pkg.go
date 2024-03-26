e277f59a860928f6a71d3d141128cf9631114dee
Artyom Pervukhin <github@artyom.dev>
2024-03-26 19:07:06 +0100

    Reimplement FuncVal as a wrapper around Func

--- pkg.go
+++ pkg.go
@@ -78,41 +78,12 @@ retryLoop:
 // useful with a non-zero delay. If the context is canceled, function returns
 // an error returned by the Context.Err method.
 func FuncVal[T any](ctx context.Context, cfg Config, fn func() (T, error)) (T, error) {
-	if cfg.RetryOn == nil || cfg.MaxAttempts < 1 {
-		return fn()
-	}
-	var err error
 	var val T
-retryLoop:
-	for i := range cfg.MaxAttempts {
-		if i != 0 {
-			if cfg.Delay > 0 || cfg.delayFn != nil {
-				delay := cfg.Delay
-				if cfg.delayFn != nil {
-					delay = max(0, cfg.delayFn(i))
-				}
-				timer := time.NewTimer(delay)
-				select {
-				case <-ctx.Done():
-					timer.Stop()
-					err = ctx.Err()
-					break retryLoop
-				case <-timer.C:
-				}
-			} else {
-				select {
-				case <-ctx.Done():
-					err = ctx.Err()
-					break retryLoop
-				default:
-				}
-			}
-		}
+	wrap := func() error {
+		var err error
 		val, err = fn()
-		if cfg.RetryOn(err) {
-			continue
-		}
-		break
+		return err
 	}
+	err := Func(ctx, cfg, wrap)
 	return val, err
 }
